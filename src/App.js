3a6f45e2cbdbfc69d7b08d17cba67f8b62c8188c
Takuto Mitsuhashi <32606918+gitackt@users.noreply.github.com>
2018-07-16 23:10:22 +0900

    Fix layouts

--- src/App.js
+++ src/App.js
@@ -7,17 +7,31 @@ import WebsiteComponent from './components/website.component';
 import AppComponent from './components/app.component';
 import ProjectComponent from './components/project.component';
 import BlogComponent from './components/blog.component';
+import WorkComponent from './components/work.component';
+import FooterComponent from './components/footer.component';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <JumbotronComponent />
+        <img
+          style={{ position: 'relative', width: '100%', height: '40px', top: '-55px', zIndex: '10000' }}
+          src="/drop.png"
+          alt={'drop'}
+        />
         <ProfileComponent />
+        <img
+          style={{ position: 'relative', width: '100%', height: '40px', top: '-130px'}}
+          src="/wave.png"
+          alt={'wave'}
+        />
         <WebsiteComponent />
         <AppComponent />
         <ProjectComponent />
+        <WorkComponent />
         <BlogComponent />
+        <FooterComponent />
       </div>
     );
   }
