334cbb330e3fa546c064f5b7d0be351d5a0ed9b9
Takuto Mitsuhashi <32606918+gitackt@users.noreply.github.com>
2018-07-16 23:11:01 +0900

    fix jumbotron.component

--- src/components/jumbotron.component.jsx
+++ src/components/jumbotron.component.jsx
@@ -5,9 +5,16 @@ class JumbotronComponent extends Component {
   render() {
     return (
       <div 
-        style={{ position: 'relative', top: -55, paddingTop: '200px', height: '400px', backgroundImage: `url('/background.jpg')`, backgroundSize: 'cover', color: 'white', backgroundPosition: 'center center'  }}
+        // style={{ position: 'relative', top: -55, paddingTop: '200px', height: '450px', backgroundImage: `url('/background.jpg')`, backgroundSize: 'cover', color: 'white', backgroundPosition: 'center center' }}
+        style={{ position: 'relative', top: -55, paddingTop: '210px', height: '440px' }}
       >
-          <h1>tackt</h1>
+        <h1>
+          <img
+            style={{ position: 'relative', width: '280px', height: 'auto' }}
+            src="/tackt.png"
+            alt={'tackt'}
+          />
+        </h1>
       </div>
     );
   }
