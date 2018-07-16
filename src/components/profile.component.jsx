e6779e3a53dc8210af83fa56687d55245112f683
Takuto Mitsuhashi <32606918+gitackt@users.noreply.github.com>
2018-07-16 23:11:20 +0900

    fix profile.component.jsx.component

--- src/components/profile.component.jsx
+++ src/components/profile.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const styles = {
   root: {
@@ -10,8 +11,58 @@ class ProfileComponent extends Component {
 
   render() {
     return (
-      <div className={styles.root} style={{ height: '300px' }}>
+      <div className={styles.root} style={{ position: 'relative', top: -95, padding: '80px', paddingBottom: '120px', height: '100%', background: '#1D1E19', color: 'white' }}>
         <h2>profile</h2>
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={2}/>
+          <Grid item xs={12} md={3} style={{ textAlign: 'center' }}>
+            <div>
+              <img
+                style={{ position: 'relative', width: '150px', height: '150px', borderRadius: '100px' }}
+                src="/profile.jpg"
+                alt={'Tackt Mitsuhashi'}
+              />
+              <h3 style={{ fontSize: '27px' }}>三橋拓人</h3>
+              <h4 style={{ fontSize: '17px' }}>Tackt Mitsuhashi</h4>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={5} style={{ textAlign: 'left' }}>
+          <p>
+            自己紹介<br/>
+            個人でWebアプリケーションの開発をやってます。コーディングだけでなくUIデザインやインフラ構築、サービス開発に関することは全て一人で行います。平面デザインやイラスト・漫画制作も行っています。
+
+            <br/>
+            <br/>
+            
+            経歴<br/>
+            2016 ~           明治大学 商学部<br/>
+            2017 ~ 2018      株式会社Whatzmoney<br/>
+            2018 ~           株式会社MICIN 
+            
+            <br/>
+            <br/>
+
+            技術<br/>
+            Python, Django, Rails, Node, React, React-native, Mysql, Nginx, Docker, Aws,
+            Affinty Designer
+
+            <br/>
+            <br/>
+            
+            資格<br/>
+            {/* 証券アナリスト, FP */}
+  
+            <br />
+            <br/>
+            
+            詳細<br/>
+            生年月日　     1997年6月30日<br/>
+            Email        git.tackt@gmail.com<br/>
+            Ethereum     0x9ddb22480bd6f3d56de73457fe657520d5da6693<br/>
+          </p>
+          </Grid>
+          <Grid item xs={12} md={2} />
+        </Grid>
       </div>
     );
   }
