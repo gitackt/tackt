30a54acb9955ea33659f63d1a66601c352dd1fdf
Takuto Mitsuhashi <picos.tackt@gmail.com>
2018-09-16 10:49:00 +0900

    Add react router sitemap

---
+++ src/router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+	<Route>
+    <Route path="/" />
+    <Route path="/engeneer" />
+    <Route path="/comic"  />
+    <Route path="/profile"  />
+    <Route path="/sns" />
+	</Route>
+);
