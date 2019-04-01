6ff57f8d0946b300526a88771d587cba85655ef6
Takuto Mitsuhashi <picos.tackt@gmail.com>
2019-04-02 00:52:13 +0900

    Remove extra image

--- src/params/gallery.js
+++ src/params/gallery.js
@@ -1,38 +1,24 @@
 export const galleryData = [
   {
     name: "TPOK in jerry",
-    number: 6,
+    number: 4,
     term: "2019/4 ~ ",
     image: "/images/gallery/tpok.png",
     info: "タピオカが入ったゼリーのイラストです。",
   },
   {
     name: "Golden baby",
-    number: 5,
+    number: 3,
     term: "2019/1 ~ 2019/3",
     image: "/images/gallery/golden.png",
     info: "金色の赤ちゃんのイラストです。",
   },
   {
     name: "Pink Twintail",
-    number: 4,
+    number: 2,
     term: "2018/11 ~ 2018/12",
     image: "/images/gallery/pink.jpg",
     info: "ピンク髪ゴシック風のイラストです。",
-  },
-  {
-    name: "Jack the ripper",
-    number: 3,
-    term: "2018/10 ~ 2018/11",
-    image: "/images/gallery/jack.png",
-    info: "切り裂きジャック風のイラストです。",
-  },
-  {
-    name: "I need job",
-    number: 2,
-    term: "2018/10 ~ 2018/11",
-    image: "/images/gallery/job.jpg",
-    info: "花を持った女性のイラストです。",
   },
   {
     name: "Pencil",
