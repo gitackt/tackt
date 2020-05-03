176448b6f958a7a552619480e054b9e415dd2aa4
Takuto Mitsuhashi <picos.tackt@gmail.com>
2020-05-04 05:21:42 +0900

    Recreate priject

--- routine.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "Fix"
-git push origin master
-yarn deploy
--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,27 @@
-import React, { Component } from 'react';
-import FooterComponent from './components/common/footer.component';
-import NavigationComponent from './components/common/navi.component';
-import ServiceComponent from './components/service.component';
-import { ProfileComponent } from './components/profile.component';
-import JumbotronComponent from './components/jumbotron.component';
+import React from "react"
+import "./App.css"
 import {
   Route,
   Switch,
   BrowserRouter as Router,
-  Redirect
-} from 'react-router-dom';
+  Redirect,
+} from "react-router-dom"
 
-const styles = {
-  background: {
-    width: '100%',
-    height: '100%'
-  }
-};
-
-class LandingComponent extends Component {
-  render() {
-    return (
-      <div style={styles.background}>
-        <NavigationComponent />
-        <JumbotronComponent />
-        <ProfileComponent />
-        <ServiceComponent />
-        <FooterComponent />
-      </div>
-    );
-  }
+const LandingComponent = () => {
+  return <div className="background">test</div>
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <Switch>
-            <Route exact={true} path="/" component={LandingComponent} />
-            <Route render={() => <Redirect to={'/'} />} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact={true} path="/" component={LandingComponent} />
+          <Route render={() => <Redirect to={"/"} />} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
